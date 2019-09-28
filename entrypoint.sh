bd7b71696aefa83e05540b2c13087c7e6d1e3965
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-09-28 12:43:06 +0800

    debug
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ set -eu
 export GITHUB="true"
 
 [ -n "$INPUT_STRIP_COMPONENTS" ] && export INPUT_STRIP_COMPONENTS=$((INPUT_STRIP_COMPONENTS + 0))
-
+echo $INPUT_STRIP_COMPONENTS
 sh -c "/bin/drone-scp $*"
