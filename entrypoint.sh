2ef3f5e459a41a73c4d767489edaa342ab146597
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-09-28 11:40:28 +0800

    chore: check timeout variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 
 export GITHUB="true"
 
-[ -n "$INPUT_TIMEOUT" ] && export INPUT_TIMEOUT="30s"
-[ -n "$INPUT_COMMAND_TIMEOUT" ] && export INPUT_COMMAND_TIMEOUT="1m"
+[ -z "$INPUT_TIMEOUT" ] && export INPUT_TIMEOUT="30s"
+[ -z "$INPUT_COMMAND_TIMEOUT" ] && export INPUT_COMMAND_TIMEOUT="1m"
 
 sh -c "/bin/drone-scp $*"
