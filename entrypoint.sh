f0de4fea6f3f2078eef10b693036f2a75319b0e4
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-09-28 11:37:34 +0800

    chore: add default timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ set -eu
 
 export GITHUB="true"
 
+[ -n "$INPUT_TIMEOUT" ] && export INPUT_TIMEOUT="30s"
+[ -n "$INPUT_COMMAND_TIMEOUT" ] && export INPUT_COMMAND_TIMEOUT="1m"
+
 sh -c "/bin/drone-scp $*"
