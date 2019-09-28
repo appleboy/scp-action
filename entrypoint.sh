722d935a90f6a8c510601e7d144c80ff76b166ea
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-09-28 12:34:38 +0800

    update INPUT_STRIP_COMPONENTS
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ export GITHUB="true"
 
 [ -z "$INPUT_TIMEOUT" ] && export INPUT_TIMEOUT="30s"
 [ -z "$INPUT_COMMAND_TIMEOUT" ] && export INPUT_COMMAND_TIMEOUT="1m"
+[ -n "$INPUT_STRIP_COMPONENTS" ] && export INPUT_STRIP_COMPONENTS=$((INPUT_STRIP_COMPONENTS + 0))
 
 sh -c "/bin/drone-scp $*"
